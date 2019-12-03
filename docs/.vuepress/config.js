785241fa06853f018153ce7e6f28abcad34be712
caoyb2018 <caoyb2018@163.com>
2019-12-03 20:51:13 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,35 @@ module.exports = {
   title: '一个前端小青年的学习笔记',
   description: '小二,上酒',
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }], 
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
   ],
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'react源码解析', link: '/react源码解析/' },
+    ],
+    // sidebar: [
+    //   {
+    //     title: 'react源码解析', // 侧边栏名称
+    //     collapsable: true, // 可折叠
+    //     children: [
+    //       '/react源码解析/', // 你的md文件地址
+    //     ]
+    //   },
+    //   {
+    //     title: 'CSS', 
+    //     collapsable: true,
+    //     children: [
+    //       '/blog/CSS/搞懂Z-index的所有细节',
+    //     ]
+    //   },
+    //   {
+    //     title: 'HTTP',
+    //     collapsable: true,
+    //     children: [
+    //       '/blog/HTTP/认识HTTP-Cookie和Session篇',
+    //     ]
+    //   },
+    // ]
+  },
 }
--- docs/README.md
+++ docs/README.md
@@ -2,7 +2,7 @@
 home: true
 heroImage: /bg9.jpg
 actionText: 快速上手 →
-actionLink: /zh/guide/
+actionLink: /react源码解析/
 # features:
 # - title: 简洁至上
 #   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
