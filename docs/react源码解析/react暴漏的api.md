364756e5ab3f09634f391e89e12d2f621dc219c1
caoyb2018 <caoyb2018@163.com>
2020-04-21 15:40:46 +0800

    增加设计模式、重写栈、队列代码

--- docs/react源码解析/react暴漏的api.md
+++ docs/react源码解析/react暴漏的api.md
@@ -1,6 +1,60 @@
-# 概念入门
+# react暴露的api
 
-## reacr.createElement
+如下
+
+``` js
+const React = {
+  Children: {
+    map,
+    forEach,
+    count,
+    toArray,
+    only,
+  },
+
+  createRef,
+  Component,
+  PureComponent,
+
+  createContext,
+  forwardRef,
+  lazy,
+  memo,
+
+  error,
+  warn,
+
+  useCallback,
+  useContext,
+  useEffect,
+  useImperativeHandle,
+  useDebugValue,
+  useLayoutEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+
+  Fragment: REACT_FRAGMENT_TYPE,
+  Profiler: REACT_PROFILER_TYPE,
+  StrictMode: REACT_STRICT_MODE_TYPE,
+  Suspense: REACT_SUSPENSE_TYPE,
+
+  createElement: __DEV__ ? createElementWithValidation : createElement,
+  cloneElement: __DEV__ ? cloneElementWithValidation : cloneElement,
+  createFactory: __DEV__ ? createFactoryWithValidation : createFactory,
+  isValidElement: isValidElement,
+
+  version: ReactVersion,
+
+  unstable_ConcurrentMode: REACT_CONCURRENT_MODE_TYPE,
+
+  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: ReactSharedInternals,
+};
+```
+
+## react.createElement
+react.createElement可以说是react里面我们用到的最多的一个api了虽然我们并没有这种感觉，但实际上每次写jsx语法我们都会用到这个方法
 ``` js
 render() {
     return (
@@ -191,3 +245,116 @@ const ReactElement = function (type, key, ref, self, source, owner, props) {
 ```
 只是返回了一个element对象，至于if (__DEV__)中等代码其实可以忽略可以理解为为了防止开发人员对element对象做一些不规范或者可能导致错误的操作。
 分析下来，react.createElement只是根据jsx语法编译后产生的参数返回了一个element对象
+
+## Componmrnt PureComponment
+``` js
+import React from 'react';
+
+export default class Hello extends React.Component {
+    ...
+}
+```
+对于react开发者，Componment这个玩意一定不会陌生因为我们写React组件的时候一定会继承这个玩意
+``` js
+
+const emptyObject = {};
+if (__DEV__) {
+  Object.freeze(emptyObject);
+}
+
+/**
+ * Base class helpers for the updating state of a component.
+ */
+function Component(props, context, updater) {
+  this.props = props;
+  this.context = context;
+  this.refs = emptyObject;
+  this.updater = updater || ReactNoopUpdateQueue;
+}
+
+Component.prototype.isReactComponent = {};
+Component.prototype.setState = function(partialState, callback) {
+  invariant(
+    typeof partialState === 'object' ||
+      typeof partialState === 'function' ||
+      partialState == null,
+    'setState(...): takes an object of state variables to update or a ' +
+      'function which returns an object of state variables.',
+  );
+  this.updater.enqueueSetState(this, partialState, callback, 'setState');
+};
+Component.prototype.forceUpdate = function(callback) {
+  this.updater.enqueueForceUpdate(this, callback, 'forceUpdate');
+};
+```
+Componment核心代码差不多就这么多。真的是非常简单。重点就是updater这个属性了。无论是setState或者forceUpdate这种可能会导致组件重新render的方法都是通过this.updater来完成的
+
+至于Purecomponment在开发中使用的频率会比Component少很多具体参考[React文档](https://zh-hans.reactjs.org/docs/react-api.html#reactpurecomponent)
+
+PureComponment不需要我们去手动实现shouldComponmentUpdate更新state和props时会自动做一次浅比较判断是否重新render但是仅仅只是做了一次浅比较如果修改的是数组或者对象的时候可能会出现一些问题，比如值已经发生改变但是页面并没有做出响应。
+``` js
+function ComponentDummy() {}
+ComponentDummy.prototype = Component.prototype;
+function PureComponent(props, context, updater) {
+  this.props = props;
+  this.context = context;
+  this.refs = emptyObject;
+  this.updater = updater || ReactNoopUpdateQueue;
+}
+
+const pureComponentPrototype = (PureComponent.prototype = new ComponentDummy());
+pureComponentPrototype.constructor = PureComponent;
+Object.assign(pureComponentPrototype, Component.prototype);
+pureComponentPrototype.isPureReactComponent = true;
+```
+PureComponment实际上是继承Conponment，典型的寄生组合式继承
+
+这两者之间的差别其实比较重要的一点就是**pureComponentPrototype.isPureReactComponent = true;**这一句给PureComponment加了一个标识
+
+## React.createRef
+react中的ref用法应该都是比较清楚的了
+
+ref={ref=testRef}
+
+ref={ref=>this.ref=ref}
+
+react.createRef
+
+上面的三种用法，其中第一种用法已经不推荐甚至快要被废弃了。
+
+react.createRef其实是一个超级简单的方法
+``` js
+export function createRef(): RefObject {
+  const refObject = {
+    current: null,
+  };
+  if (__DEV__) {
+    Object.seal(refObject);
+  }
+  return refObject;
+}
+```
+就是返回了一个对象，对象的结构很清晰了。当使用<div ref={this.ref} />来绑定ref会给current这个对象赋值一个对象。通过操作current来操作这个组件。
+
+## React.forwardRef
+
+forwardRef这个api用的也比较少具体可参考文档[](https://zh-hans.reactjs.org/docs/react-api.html#reactforwardref)
+日常中用的比较多的其实也就是给函数组件绑定ref因为在函数式组件中是没有实例的。不能像class声明的组件直接绑定ref。但是我们可以通过这个组件来实现
+``` js
+const TargetComponent = React.forwardRef((props, ref) => (
+  <TargetComponent ref={ref} />
+))
+```
+这样别人在使用这个组件的时候可以像class组件一样使用ref
+forwardRef的api也很简单再删除if(_Dev_)的代码后就这么多
+``` js
+export default function forwardRef<Props, ElementType: React$ElementType>(
+  render: (props: Props, ref: React$Ref<ElementType>) => React$Node,
+) {
+  return {
+    $$typeof: REACT_FORWARD_REF_TYPE,
+    render,
+  };
+}
+```
+核心就是添加了一个$$typeof的标识
--- test1.js
+++ test1.js
@@ -1,17 +1,314 @@
-var createDiv = (function(){
-    var instance = null;
-    createDiv = function(html){
-        if(!instance) {
-            this.html = html;
-            this.init();
-            instance = this;
+//单例模式代码
+// var Storage = (function(){
+//     var instance = null;
+//     var obj = {}
+//     Storage = function(key, value){
+//         if(!instance) {
+//             this.init(key, value);
+//             instance = this;
+//         }
+//         return instance;
+//     }
+//     Storage.prototype.init = function(key, value){
+//         instance.setItem() = function(){
+//             instance[key] = value
+//         }
+//         instance.getItem() = function(){
+//             return instance[key]
+//         }
+//     }
+//     return Storage;
+// })()
+
+// var CreateDiv = function(html) {
+//     this.html = html;
+//     this.init()
+// }
+
+// CreateDiv.prototype.init = function() {
+//     var div = document.createElement('div');
+//     div.innerHTML = this.html;
+//     document.body.appendChild(div)
+// }
+
+// var ProxySingletonCreateDiv = (function() {
+//     var instance = null;
+//     return function(html) {
+//         if(!instance) {
+//             instance = new CreateDiv(html)
+//         }
+//         return instance;
+//     }
+// })()
+
+// var div = function(html){
+//     var div = document.createElement('div');
+//     div.innerHTML=html;
+//     return div
+// }
+
+// var button = function(html){
+//     var button = document.createElement('button');
+//     button.innerHTML=html;
+//     return button
+// }
+
+// var getSingle = (function(){
+//     var result = null;
+//     return function(html) {
+//         if(!result){
+//            result = fn.apply(this, arguments)
+//         }
+//         return result
+//     }
+// })()
+
+//策略模式代码
+
+// var calculateBonus = function( performanceLevel, salary ) {
+//     if(performanceLevel === 'S') {
+//         return salary*4;
+//     }else if(performanceLevel === 'A') {
+//         return salary*3;
+//     }else if(performanceLevel === 'B') {
+//         return salary*2
+//     }
+// }
+
+// var performanceS = {
+//     calculate: function(salary) {
+//         return salary*4
+//     }
+// }
+
+// var performanceA = {
+//     calculate: function(salary) {
+//         return salary*3
+//     }
+// }
+
+// var performanceB = {
+//     calculate: function(salary) {
+//         return salary*2
+//     }
+// }
+
+// var Bonus = function(){
+//     this.salary = null;
+//     this.startegy = null;
+// }
+
+// Bonus.prototype.setSalary = function(salary) {
+//     this.salary = salary
+// }
+
+// Bonus.prototype.setStartegy = function(startegy) {
+//     this.startegy = startegy;
+// }
+
+// Bonus.prototype.getBonus = function(){
+//     if(!this.salary || !this.startegy){
+//         throw new Error()
+//     }
+//     return this.startegy.calculate(this.salary)
+// }
+
+// var startegy = {
+//     "S": function(salary){
+//         return salary*4
+//     },
+//     "A": function(salary){
+//         return salary*3
+//     },
+//     "B": function(salary){
+//         return salary*2
+//     }
+// }
+
+// var calculateBonus = function(level, salary) {
+//     return startegy[level](salary)
+// }
+
+//代理模式代码
+// var Flower = function(){};
+
+// var xiaoming = {
+//     sendFlower: function(target) {
+//         var flower = new Flower();
+//         target.receiveFlower(flower)
+//     }
+// }
+
+// var A = {
+//     receiveFlower: function(flower){
+//         console.log(`get${flower}`)
+//     }
+// }
+
+// xiaoming.sendFlower(A)
+
+// var Flower = function(){};
+
+// var xiaoming = {
+//     sendFlower: function(target) {
+//         var flower = new Flower();
+//         target.receiveFlower(flower)
+//     }
+// }
+
+// var B = {
+//     receiveFlower: function(flower) {
+//         A.receiveFlower(flower)
+//     }
+// }
+
+// var A = {
+//     receiveFlower: function(flower){
+//         console.log(`get${flower}`)
+//     }
+// }
+
+// xiaoming.sendFlower(B)
+
+// var Flower = function(){};
+
+// var xiaoming = {
+//     sendFlower: function(target) {
+//         var flower = new Flower();
+//         target.receiveFlower(flower)
+//     }
+// }
+
+// var B = {
+//     receiveFlower: function(flower) {
+//         A.listenGoodMood(function(){
+//             A.receiveFlower()
+//         })
+//     }
+// }
+
+// var A = {
+//     receiveFlower: function(flower){
+//         console.log(`get${flower}`)
+//     },
+//     listenGoodMood: function(fn) {
+//         setTimeout(function(){
+//             fn()
+//         }, 1000)
+//     }
+// }
+
+// xiaoming.sendFlower(B)
+
+// var myImage = (function(){
+//     var imgNode = document.createElement('img');
+//     document.body.appendChild(imgNode);
+
+//     return {
+//         setSrc: function(src) {
+//             imgNode.src = src
+//         }
+//     }
+// })()
+
+// var proxyImage =(function(){
+//     var img = new Image;
+//     img.onload = function(){
+//         myImage.src= this.src
+//     }
+//     return {
+//         setSrc: function(src){
+//             myImage.setSrc('loading.gif')
+//             img.src= src
+//         }
+//     }
+// })()
+
+// proxyImage.setSrc('test.png')
+
+{/* <body>
+    <input type='checkbox' id='1'></input>1
+    <input type='checkbox' id='2'></input>2
+    <input type='checkbox' id='3'></input>3
+    <input type='checkbox' id='4'></input>4
+    <input type='checkbox' id='5'></input>5
+    <input type='checkbox' id='6'></input>6
+    <input type='checkbox' id='7'></input>7
+    <input type='checkbox' id='8'></input>8
+    <input type='checkbox' id='9'></input>9
+    <input type='checkbox' id='10'></input>10
+</body>
+
+var requestCheckBox = function(id){
+    console.log(`${id}开始请求`)
+}
+
+var proxyRequestCheckBox = (function(){
+    var cache = []
+    var timer = null;
+    return function(id) {
+        cache.push(id)
+        if(timer){
+            return
         }
-        return instance;
+        timer = setTimeout(function(){
+            requestCheckBox(cache.join(','))
+            clearTimeout(timer)
+            timer=null
+            cache=[]
+        },2000)
     }
-    createDiv.prototype.init = function(){
-        var div = document.createElement('div');
-        div.innerHTML = this.html;
-        document.body.appendChild(div);
-    }
-    return createDiv;
 })()
+
+var checkbox = document.getElementsByTagName('input');
+
+for(var i=0, c; c=checkbox[ i++ ];){
+    c.onclick = function(){
+        if(this.checked === true ) {
+            proxyRequestCheckBox(this.id)
+        }
+    }
+} */}
+
+// var mult = function(){ //乘积
+//     var a= 1;
+//     for(var i=0, l= arguments.length; i<l; i++) {
+//         a= a* arguments[i]
+//     }
+//     return a;
+// }
+
+// var plus = function(){//加和
+//     var a=0;
+//     for(var i=0, l=arguments.length; i<l; i++) {
+//         a= a+ arguments[i]
+//     }
+//     return a;
+// }
+
+// var createProxyFactory = function(fn){
+//     var cache = {};
+//     return function(){
+//         var args = Array.prototype.join.call( arguments, ',');
+//         if(args in cache){
+//             return cache[args]
+//         }
+//         return cache[args] = fn.apply(this, arguments)
+//     }
+// }
+
+// var proxyMult = createProxyFactory(mult);
+// var proxyPlus = createProxyFactory(plus);
+
+// proxyMult(1,2,3,4)
+// proxyMult(1,2,3,4)
+// proxyPlus(1,2,3,4)
+// proxyPlus(1,2,3,4)
+
+//迭代器模式代码
+
+[1,2,3,4].map((item, index)=>{
+    console.log(`下标${index}`)
+    console.log(`值${item}`)
+})
+
